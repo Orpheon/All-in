c1cba4074c080285860e0f56bd8b3097c7abde5a
Jens Kaminsky <jens.kaminsky@students.fhnw.ch>
2020-03-12 02:09:51 +0100

    changed fit new agent structure, made more generic

--- Runner.py
+++ Runner.py
@@ -1,42 +1,42 @@
 import random
 
-from pypokerengine.players import BasePokerPlayer
-
-from agent.BeginnerPlayer import BeginnerPlayer
-from baseline.BaselinePokerPlayer import BaselinePlayer
-from baseline.CallBaselinePokerPlayer import CallBaselinePlayer
-from baseline.RandomPokerPlayer import RandomPlayer
 from configuration.CashGameConfig import CashGameConfig
+
+from agent.allnothing.AllNothingAgent import AllNothingAgent
+from agent.call.CallAgent import CallAgent
+from agent.random.RandomAgent import RandomAgent
+
 from league.Rating import Rating
 
 
 class Runner:
-
-  _all_players = [BeginnerPlayer(0.5, 0.5),
-                  BeginnerPlayer(0.5, 0.3),
-                  BeginnerPlayer(0.5, 0.0),
-                  BeginnerPlayer(0.3, 0.0),
-                  BeginnerPlayer(0.0, 0.0),
-                  #BaselinePlayer(),
-                  CallBaselinePlayer(),
-                  RandomPlayer()]
   _rating = Rating('./runner_placings.json')
 
-  def run_games(self, hands):
-    poker_config = CashGameConfig(evaluations=hands)
+  def __init__(self, all_agent_types, ):
+    self._all_agent_types = all_agent_types
 
-    players = self.generate_matchup()
-    for player in players:
-      poker_config.register_player(player.id, player)
-    placings = poker_config.run_evaluation()
-    self._rating.update_from_placings(placings)
+  def run_random_games(self, matchups, hands):
+
+    for i in range(matchups):
+      print('progress {0}/{1}'.format(i, matchups))
+
+      poker_config = CashGameConfig(evaluations=hands)
+      agents = self.generate_matchup()
+      for agent in agents:
+        poker_config.register_player(name=str(agent), algorithm=agent)
+      placings = poker_config.run_evaluation()
+      self._rating.update_from_placings(placings)
 
   def generate_matchup(self):
-    #TODO: make not hard coded
-    return random.sample(self._all_players, 6)
+    available_agents = [(a_type, a_id) for a_type in self._all_agent_types for a_id in a_type.available_agent_ids()]
+    picked_agents = random.sample(available_agents, 6)
+    return [a_type.from_id(a_id) for a_type, a_id in picked_agents]
+
 
 if __name__ == '__main__':
-  runner = Runner()
-  for i in range(20):
-    runner.run_games(20000)
+  all_agent_types = [CallAgent, RandomAgent, AllNothingAgent]
+  different_matches = 5
+
+  runner = Runner(all_agent_types)
+  runner.run_random_games(matchups=5, hands=10_000)
   runner._rating.plot_history()
