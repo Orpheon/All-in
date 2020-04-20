6c718ae8c7902f3d397e520bd02bd50e472d3d55
Orpheon <alex.liebendorf@gmail.com>
2020-04-20 21:43:55 +0200

    Finished SAC agent, with a lot of copy-pasting from SpinningUp.

--- agent/sac1/replaybuffer.py
+++ agent/sac1/replaybuffer.py
@@ -21,14 +21,26 @@ class ReplayBuffer:
     self.done_buf = np.zeros(size, dtype=np.float32)
     self.ptr, self.size, self.max_size = 0, 0, size
 
-  def store(self, obs, act, rew, next_obs, done):
-    self.obs_buf[self.ptr] = obs
-    self.obs2_buf[self.ptr] = next_obs
-    self.act_buf[self.ptr] = act
-    self.rew_buf[self.ptr] = rew
-    self.done_buf[self.ptr] = done
-    self.ptr = (self.ptr+1) % self.max_size
-    self.size = min(self.size+1, self.max_size)
+  def store(self, obs, act, rew, next_obs, done, batch_size):
+    self.obs_buf[self.ptr:self.ptr+batch_size] = obs
+    self.obs2_buf[self.ptr:self.ptr+batch_size] = next_obs
+    self.act_buf[self.ptr:self.ptr+batch_size] = act
+    self.rew_buf[self.ptr:self.ptr+batch_size] = rew
+    self.done_buf[self.ptr:self.ptr+batch_size] = done
+    self.ptr += batch_size
+    if self.ptr + batch_size > self.max_size:
+      rng_state = np.random.get_state()
+      np.random.shuffle(self.obs_buf)
+      np.random.set_state(rng_state)
+      np.random.shuffle(self.obs2_buf)
+      np.random.set_state(rng_state)
+      np.random.shuffle(self.act_buf)
+      np.random.set_state(rng_state)
+      np.random.shuffle(self.rew_buf)
+      np.random.set_state(rng_state)
+      np.random.shuffle(self.done_buf)
+    self.ptr = (self.ptr+batch_size) % self.max_size
+    self.size = min(self.size+batch_size, self.max_size)
 
   def sample_batch(self, batch_size=32):
     idxs = np.random.randint(0, self.size, size=batch_size)
