c7cf56af5202fe209388679e28327446e9e83a05
Jens Kaminsky <jens.kaminsky@students.fhnw.ch>
2020-05-04 16:47:30 +0200

    adapted call agent for np engine

--- agent/call/callAgentNP.py
+++ agent/call/callAgentNP.py
@@ -1,7 +1,8 @@
-from agent.baseAgentNP import BaseAgentNP
+from agent.baseAgentLoadable import BaseAgentLoadable
 
+import numpy as np
 
-class CallAgentNP(BaseAgentNP):
+class CallAgentNP(BaseAgentLoadable):
 
   def __str__(self):
     return 'CallAgent_{0}'.format(self.agent_id)
@@ -10,12 +11,8 @@ class CallAgentNP(BaseAgentNP):
   def _config_file_path(cls):
     return './agent/call/config.json'
 
-  def declare_action(self, valid_actions, hole_card, round_state):
-    call_action_info = valid_actions[1]
-    action, amount = call_action_info["action"], call_action_info["amount"]
-    return action, amount
-
   def act(self, player_idx, round, current_bets, min_raise, prev_round_investment, folded, last_raiser, hole_cards, community_cards):
-    actions = 1
-    amounts = 0
+    batch_size = current_bets.shape[0]
+    actions = np.array([0]*batch_size)
+    amounts = np.array([0]*batch_size)
     return actions, amounts
