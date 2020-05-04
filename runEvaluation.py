9cea9ed44f6997a22e84cce369774e84caa20c4d
Jens Kaminsky <jens.kaminsky@students.fhnw.ch>
2020-05-04 23:26:17 +0200

    added wrapper for pypokerengine

--- runEvaluation.py
+++ runEvaluation.py
@@ -8,13 +8,13 @@ import pickle
 import numpy as np
 import treys
 
+from agent.pypokerAgentAdapter import PypokerAgentAdapter
+from agent.random.randomAgentNP import RandomAgentNP
+
 N_TESTCASES = 10000
 
 from baseline.consolePokerPlayer import ConsolePlayer
 from configuration.cashGameConfig import CashGameConfig
-from agent.myBot import MyBotPlayer
-from agent.leagueTestBot import LeagueTestBot
-from agent.random.randomAgentPyPoker import RandomAgent
 
 parser = argparse.ArgumentParser(description='Run poker evaluation')
 parser.add_argument('--config', help='Config file')
@@ -58,13 +58,11 @@ if __name__ == '__main__':
   else:
     # Use this for manual evaluation
     poker_config = CashGameConfig(evaluations=N_TESTCASES, log_file_location="leaguetestlog.json")
-    poker_config.register_player("RandomAgent", RandomAgent(1, ''))
-    poker_config.register_player("RandomAgent", RandomAgent(2, ''))
-    poker_config.register_player("RandomAgent", RandomAgent(3, ''))
-    poker_config.register_player("RandomAgent", RandomAgent(4, ''))
-    poker_config.register_player("RandomAgent", RandomAgent(5, ''))
-    poker_config.register_player("RandomAgent", RandomAgent(6, ''))
-    # poker_config.add_all_available_baselines()
+    #poker_config.add_all_available_baselines()
+
+    players = (PypokerAgentAdapter(RandomAgentNP(None, None)) for _ in range(6))
+    for i, p in enumerate(players):
+      poker_config.register_player(name='RandomAgentNp_{0}'.format(i), algorithm=p)
 
   print(f"Start evaluating {poker_config.evaluations} hands")
   start = time.time()
@@ -77,45 +75,3 @@ if __name__ == '__main__':
   community_cards = np.ndarray((N_TESTCASES, 5), dtype=int)
   hole_cards = np.ndarray((N_TESTCASES, 6, 2), dtype=int)
   gains = np.ndarray((N_TESTCASES, 6), dtype=int)
-
-  # with open("leaguetestlog.json", "r") as f:
-  #   results = json.load(f)
-  # # Apparently stuff is sorted by keys here, I do not have the slightest idea why
-  #
-  # with open("leaguetestlog_readable.json", "w") as f:
-  #   json.dump(results, f, sort_keys=True, indent=2)
-  #
-  # for k,v in results.items():
-
-
-
-  # old_cashgame = {}
-
-  # for round_idx, filepath in enumerate(filelist):
-  #   with open(os.path.join("testcases", filepath), "rb") as f:
-  #     data = pickle.load(f)
-  #     players = data['seats']
-  #     # Seat rotation
-  #     players = players[data['small_blind_pos']:] + players[:data['small_blind_pos']]
-  #     # Log items of interest
-  #     community_cards[round_idx] = np.array([treys.Card.new(x) for x in data['community_cards']])
-  #     for player_idx, player in enumerate(players):
-  #       hole_cards[round_idx, player_idx] = np.array([treys.Card.new(x) for x in player['hole_card']])
-  #       if player['uuid'] in old_cashgame:
-  #         gains[round_idx - 1, player_idx] = player['cashgame_stack'] - old_cashgame[player['uuid']]
-  #       old_cashgame[player['uuid']] = player['cashgame_stack']
-  #
-  #     # print(json.dumps(data, sort_keys=True, indent=2))
-  #     # print(old_cashgame)
-  #     # input()
-  # print(gains.sum(axis=0))
-  # # with open("testcases.pickle", "wb") as f:
-  # #   pickle.dump(data, f)
-  # shutil.rmtree("testcases")
-
-
-  if args.store_result:
-    with open('result.txt', 'w+') as fp:
-      pretty_result = '\n'.join(f"{rank + 1:2}. Player: {name:>25}, Stack: {stack:n}" for rank, (name, stack) in
-                    enumerate(result.items()))
-      fp.write(pretty_result)
