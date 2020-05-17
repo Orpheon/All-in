31726c51f7812f2dc8a4ea2ccf6765047d8d1130
bbbot2015 <jens.kaminsky@web.de>
2020-05-18 01:52:00 +0200

    added cashflow plotting for new logger

--- logPlotter.py
+++ logPlotter.py
@@ -27,6 +27,41 @@ class LogfileCollector:
 
   root_path = './gamelogs/'
   game_winnings = []
+  cashflows = []
+
+  def plot_game_winnings(self):
+    players = set()
+    for game_winning in self.game_winnings:
+      for p in game_winning.keys():
+        players.add(p)
+
+    for p in players:
+      winnings = [gw.get(p, 0) for gw in self.game_winnings]
+      plt.plot(winnings, label=p, marker='*')
+    plt.legend()
+    plt.xlabel('game index')
+    plt.ylabel('winning')
+    plt.show()
+
+  def plot_cashflow(self):
+    for idx, game in enumerate(self.cashflows):
+      cashflow_list = []
+      for spender_idx in range(6):
+        for receiver_idx in range(6):
+          cashflow_list.append([game[0][spender_idx], game[0][receiver_idx], int(game[1][receiver_idx][spender_idx])])
+
+      cashflow_data = pandas.DataFrame(np.array(cashflow_list))
+      cashflow_data.columns = ['spender', 'receiver', 'amount']
+      cashflow_data['spender'] = cashflow_data['spender'].astype('category')
+      cashflow_data['receiver'] = cashflow_data['receiver'].astype('category')
+      cashflow_data['amount'] = cashflow_data['amount'].astype('int64')
+      cashflow_pivot = cashflow_data.pivot('spender', 'receiver', 'amount')
+
+      f, ax = plt.subplots(figsize=(9, 6))
+      sns.heatmap(cashflow_pivot, annot=True, fmt="d", linewidths=.5, ax=ax, cmap='Blues')
+      plt.tight_layout()
+      plt.title('game {}/{}'.format(idx+1, len(self.cashflows)))
+      plt.show()
 
   def load_n_most_recent_logfiles(self, n):
     file_names = [self.root_path + fn for fn in listdir(self.root_path) if isfile(self.root_path + fn) and fn.endswith('bz2')]
@@ -39,14 +74,37 @@ class LogfileCollector:
         event_code = event[0]
         print(type(event_code))
         if event_code == constants.EV_END_GAME:
-          self.handle_event_ev_end_game(event[1:])
+          self._handle_event_ev_end_game(event[1:])
 
-  def handle_event_ev_end_game(self, data):
-    #card_values = data[0]
+  def _handle_event_ev_end_game(self, data):
+    card_values = data[0]
     winnings = data[1]
     players = data[2]
     winnings_sum = np.sum(winnings, NP_VERTICAL)
     self.game_winnings.append({p: w for p, w in zip(players, winnings_sum)})
+
+    receivers = winnings > 0
+    spenders = winnings < 0
+
+    no_receivers = np.sum(receivers, NP_HORIZONTAL).reshape(len(receivers), 1)
+
+    zero_games = []
+    for i, nsp in enumerate(no_receivers):
+      if nsp[0] == 0:
+        zero_games.append(i)
+        print(i, winnings[i], card_values[i])
+
+    clean_receivers = np.delete(receivers, zero_games, axis=NP_VERTICAL)
+    clean_winnings = np.delete(winnings, zero_games, axis=NP_VERTICAL)
+    clean_spenders = np.delete(spenders, zero_games, axis=NP_VERTICAL)
+    clean_no_receivers = np.delete(no_receivers, zero_games, axis=NP_VERTICAL)
+
+    spendings_each = clean_spenders * clean_winnings * -1 / clean_no_receivers
+    spendings_each = np.floor(spendings_each)
+
+    cashflow_matrix = clean_receivers.transpose().dot(spendings_each)
+    self.cashflows.append((players, cashflow_matrix))
+
 
   def load_logfile(self, file_path):
     with bz2.BZ2File(file_path, 'r') as f:
@@ -79,19 +137,13 @@ def cardval_from_str(hole):
 if __name__ == '__main__':
 
   logfile_collector = LogfileCollector()
-  logfile_collector.load_n_most_recent_logfiles(5)
+  logfile_collector.load_n_most_recent_logfiles(1)
 
   game_winnings = logfile_collector.game_winnings
-  players = set()
-  for game_winning in game_winnings:
-    for p in game_winning.keys():
-      players.add(p)
 
-  for p in players:
-    winnings = [gw.get(p, 0) for gw in game_winnings]
-    plt.plot(winnings, label=p)
-  plt.legend()
-  plt.show()
+  logfile_collector.plot_game_winnings()
+  logfile_collector.plot_cashflow()
+
 
   #TODO:
   exit()
