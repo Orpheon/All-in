b13c2fc5b80375c30829592f9a02e5e8d3ef68a0
bbbot2015 <jens.kaminsky@web.de>
2020-06-15 23:13:02 +0200

    fixed fold agent

--- agent/fold/foldAgentNP.py
+++ agent/fold/foldAgentNP.py
@@ -5,7 +5,8 @@ from agent.baseAgentLoadable import BaseAgentLoadable
 
 class FoldAgentNP(BaseAgentLoadable):
 
-  def act(self, player_idx, round, current_bets, min_raise, prev_round_investment, folded, last_raiser, hole_cards, community_cards):
+  def act(self, player_idx, round, active_rounds, current_bets, min_raise, prev_round_investment, folded, last_raiser,
+          hole_cards, community_cards):
     actions = np.zeros_like(min_raise)
     amounts = np.zeros_like(min_raise)
     return actions, amounts
