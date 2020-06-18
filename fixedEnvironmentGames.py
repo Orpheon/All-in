2138394bda24a9c0d278c900101b780eb31e4720
bbbot2015 <jens.kaminsky@web.de>
2020-06-18 23:42:59 +0200

    improved fixedEnvironment parametrization

--- fixedEnvironmentGames.py
+++ fixedEnvironmentGames.py
@@ -4,8 +4,11 @@ from collections import namedtuple
 import pandas
 
 from agent.baseAgentNP import BaseAgentNP
+from agent.call.callAgentNP import CallAgentNP
 from agent.fold.foldAgentNP import FoldAgentNP
-from agent.qlearn3.qlearn3AgentNP import Qlearn3AgentNP
+from agent.qlearn1.qlearn1AgentNP import Qlearn1AgentNP
+from agent.qlearn2.qlearn2AgentNP import Qlearn2AgentNP
+from agent.random.randomAgentNP import RandomAgentNP
 from league.game import GameEngine
 from league.logger import GenericLogger
 from testing.mockGameEngine import MockGameEngine
@@ -34,6 +37,7 @@ def generate_sorted_deck(batch_size, n_players):
   cards = np.array([FULL_DECK for _ in range(batch_size)])
   community_cards = cards[:, :5]
   hole_cards = np.reshape(cards[:, 5:5 + 2 * n_players], (batch_size, n_players, 2))
+  hole_cards = np.array([[[134224677, 134253349] for _ in range(n_players)] for _ in range(batch_size)])
   return community_cards, hole_cards
 
 
@@ -48,6 +52,9 @@ class AgentActionLogger(BaseAgentNP):
     super().__init__()
     self._agent = agent
     self.actions = []
+
+  def __str__(self):
+    return 'AALogger({})'.format(str(self._agent))
 
   def act(self, player_idx, round, active_rounds, current_bets, min_raise, prev_round_investment, folded, last_raiser,
           hole_cards, community_cards):
@@ -90,7 +97,7 @@ def plot_sns_heatmap(data_pivot, title, save_to_file):
 
 
 def plot_curves(x, ys, lbls, title, save_to_file):
-  #TODO: remove prints
+  # TODO: remove prints
   for y, lbl in zip(ys, lbls):
     plt.plot(x, y, label=lbl)
   plt.title(title)
@@ -125,12 +132,22 @@ def plot_hand_strength_bet_distribution(agentActionLogger: AgentActionLogger):
     for k in a.keys():
       available_amounts.add(k)
   hand_strengths_x = sorted(list(action_tables.keys()))
-  action_ys = [[action_tables[hs].get(aa, 0)/sum(action_tables[hs].values())
+  action_ys = [[action_tables[hs].get(aa, 0) / sum(action_tables[hs].values())
                 for hs in hand_strengths_x]
                for aa in available_actions]
-  amount_ys = [[amount_tables[hs].get(aa, 0)/sum(amount_tables[hs].values())
+  amount_ys = [[amount_tables[hs].get(aa, 0) / sum(amount_tables[hs].values())
                 for hs in hand_strengths_x]
                for aa in available_amounts]
+
+  print('-' * 50)
+  for i, e in zip(available_actions, [[action_tables[hs].get(aa, 0) for hs in hand_strengths_x]
+                                      for aa in available_actions]):
+    print(i, sum(e))
+  print('-' * 50)
+  for i, e in zip(available_amounts, [[amount_tables[hs].get(aa, 0) for hs in hand_strengths_x]
+                                      for aa in available_amounts]):
+    print(i, sum(e))
+  print('-' * 50)
 
   plot_curves(hand_strengths_x, action_ys, available_actions, 'fixed_environment_action_per_hs', False)
   plot_curves(hand_strengths_x, amount_ys, available_amounts, 'fixed_environment_amount_per_hs', False)
@@ -163,7 +180,7 @@ def plot_hand_strength_bet_distribution(agentActionLogger: AgentActionLogger):
 
 if __name__ == '__main__':
   # config
-  N_TESTCASES = 10000
+  N_TESTCASES = 10_000
   INITIAL_CAPITAL = 200
   SMALL_BLIND = 1
   BIG_BLIND = 2
@@ -171,24 +188,47 @@ if __name__ == '__main__':
 
   N_RUNS = 1
 
+  print('started with following config:')
+  print('N_TESTCASES:    ', N_TESTCASES)
+  print('INITIAL_CAPITAL:', INITIAL_CAPITAL)
+  print('SMALL_BLIND:    ', SMALL_BLIND)
+  print('BIG_BLIND:      ', BIG_BLIND)
+  print('N_PLAYERS       ', N_PLAYERS)
+  print('N_RUNS:         ', N_RUNS)
+
+
   community_cards, hole_hands = generate_sorted_deck(N_TESTCASES, N_PLAYERS)
   logger = GenericLogger()
 
-  #mock_game_engine = MockGameEngine(N_TESTCASES, INITIAL_CAPITAL, SMALL_BLIND, BIG_BLIND, community_cards, hole_hands,
+  # mock_game_engine = MockGameEngine(N_TESTCASES, INITIAL_CAPITAL, SMALL_BLIND, BIG_BLIND, community_cards, hole_hands,
   #                                  logger)
 
   mock_game_engine = GameEngine(N_TESTCASES, INITIAL_CAPITAL, SMALL_BLIND, BIG_BLIND, logger)
 
-  qlearning_agent = Qlearn3AgentNP.from_id('Qlearn3-34362883')
-  agentActionLogger = AgentActionLogger(qlearning_agent)
+  qlearning_agent = Qlearn2AgentNP.from_id('Qlearn2-55433380')
+  aALogger = AgentActionLogger(qlearning_agent)
 
-  players = [FoldAgentNP('Dummy', None) for _ in range(3)] + \
-            [agentActionLogger] + \
-            [FoldAgentNP('Dummy', None) for _ in range(2)]
+  players = [RandomAgentNP('Rng-{}'.format(i), None) for i in range(3)] + [CallAgentNP('Cll-{}'.format(i), None) for i in range(2)] + [aALogger]
+
+  # TODO: reenable
+  # players = [FoldAgentNP('Dmy', None) for _ in range(3)] + [aALogger] + [FoldAgentNP('Dmy', None) for _ in range(2)]
 
   for _ in range(N_RUNS):
-    print('-' * 100)
-    results = mock_game_engine.run_game(players)
-  print('-' * 100)
+    cumulated_winnings = {str(p): [-1 for _ in range(6)] for p in players}
+    for seat_shift in range(0, 6):
+      players = players[1:] + players[:1]
+      total_winnings = mock_game_engine.run_game(players)
+      winnings = np.sum(total_winnings, axis=0).tolist()
 
-  plot_hand_strength_bet_distribution(agentActionLogger)
+      for s, (p, w) in enumerate(zip(players, winnings)):
+        cumulated_winnings[str(p)][s] = w / N_TESTCASES
+
+    print('-' * 100)
+    print('winnings:')
+    print('{:<30}: {:20} | {:7} {:7} {:7} {:7} {:7} {:7}'.format('name', 'total winning', 'SB', 'BB', 'EP', 'MP', 'CO', 'BTN'))
+    print('-' * 100)
+    for p, sw in cumulated_winnings.items():
+      print('{:<30}: {:20} | {:7} {:7} {:7} {:7} {:7} {:7}'.format(str(p), sum(sw), sw[0], sw[1], sw[2], sw[3], sw[4], sw[5]))
+    print('-' * 100)
+
+#  plot_hand_strength_bet_distribution(aALogger)
