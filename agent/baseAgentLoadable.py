7efecd2091d6e9b688da19b9534d76b3b328e545
Jens <jens.kaminsky@students.fhnw.ch>
2020-06-23 03:32:09 +0200

    fixed baseAgentLoadable

--- agent/baseAgentLoadable.py
+++ agent/baseAgentLoadable.py
@@ -11,27 +11,30 @@ class BaseAgentLoadable(BaseAgentNP):
     self.config = config
 
   def __str__(self):
-    return self.agent_id
+    if not self.config['setup']['trainable']:
+      return self.agent_id
+    else:
+      return '{}-GEN{:03}'.format(self.agent_id, self.config['setup']['generation'])
+
+  def save_config(self):
+    with open(self._config_file_path(), 'r') as f:
+      config_data = json.load(f)
+    config_data['agent_ids'][self.agent_id] = self.config
+    with open(self._config_file_path(), 'w') as f:
+      json.dump(config_data, f, indent=2, sort_keys=True)
 
   @classmethod
   def available_agents(cls):
     with open(cls._config_file_path(), 'r') as f:
       config = json.loads(f.read())
-    return [(agent_id, agent_info['matchup_info']) for agent_id, agent_info in config['agent_ids'].items()]
+    return [(agent_id, agent_info) for agent_id, agent_info in config['agent_ids'].items()]
 
   @classmethod
   def from_id(cls, agent_id):
     with open(cls._config_file_path(), 'r') as f:
-      config = json.loads(f.read())
-    setup = config['agent_ids'][agent_id]['setup']
-    return cls(agent_id, setup)
-
-  @classmethod
-  def retire(cls, agent_id):
-    with open(cls._config_file_path(), 'rw') as f:
-      config = json.loads(f.read())
-      config['agent_ids'][agent_id]['setup']['type'] = 'retired'
-      f.write(json.dumps(config))
+      all_config = json.loads(f.read())
+    config = all_config['agent_ids'][agent_id]
+    return cls(agent_id, config)
 
   @classmethod
   def _config_file_path(cls):
