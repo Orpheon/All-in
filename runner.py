3f4f9e8ccffe0594d180168da70464a5572f48e4
Jens <jens.kaminsky@students.fhnw.ch>
2020-07-03 20:01:50 +0200

    removed old rating, updated runner to new system

--- runner.py
+++ runner.py
@@ -1,11 +1,24 @@
 import random
 
 from league.leaderboard import Leaderboard
-from league.division import NormalDivision
+from league.division import RandomDivision, OverfitDivision, PermaEvalChoiceDivision, PermaEvalSampleDivision
 
 from league.game import GameEngine
 from league.logger import GenericLogger, NoneLogger
 from league.agentManager import AgentManager
+
+
+def add_all_students(agent_manager, division, students=('qlearn1', 'qlearn2', 'qlearn3')):
+  for s in students:
+    id = agent_manager.add_agent(s, True)
+    division.state['students'].append(id)
+
+
+def add_all_baselines(agent_manager, division, baselines=('call', 'random')):
+  for b in baselines:
+    id = agent_manager.add_agent(b, False)
+    division.state['teachers'].append(id)
+
 
 if __name__ == '__main__':
   BATCH_SIZE = 10_000
@@ -14,144 +27,100 @@ if __name__ == '__main__':
   BIG_BLIND = 2
   N_PLAYERS = 6
 
-  N_ROUNDS_TO_CLONE = 100
-
+  N_ROUNDS_TO_CLONE = 200
+  N_ROUNDS_TO_UPDATE_LEADERBOARD = 50
   # logger = GenericLogger()
+
+  #
+  # basic setup
+  #
+
   logger = NoneLogger()
 
   game_engine = GameEngine(BATCH_SIZE, INITIAL_CAPITAL, SMALL_BLIND, BIG_BLIND, logger)
-
   agent_manager = AgentManager(file_path='./savefiles/agent_manager.json', models_path='./models')
-  leaderboard_1 = Leaderboard(file_path='./savefiles/leaderboard_1.json')
-  division_1 = NormalDivision('./savefiles/league_one.json', game_engine, leaderboard_1, agent_manager)
 
-  # add_all_agents(agent_manager)
+  # create leaderboards
+  leaderboard_random_1 = Leaderboard(file_path='./savefiles/leaderboard_random_1.json')
+  leaderboard_random_2 = Leaderboard(file_path='./savefiles/leaderboard_random_2.json')
+  leaderboard_random_3 = Leaderboard(file_path='./savefiles/leaderboard_random_3.json')
+  leaderboard_random_4 = Leaderboard(file_path='./savefiles/leaderboard_random_4.json')
+  leaderboard_random_5 = Leaderboard(file_path='./savefiles/leaderboard_random_5.json')
+  leaderboard_random_6 = Leaderboard(file_path='./savefiles/leaderboard_random_6.json')
+  leaderboard_perma_eval_choice = Leaderboard(file_path='./savefiles/leaderboard_perma_eval_choice.json')
+  leaderboard_perma_eval_sample = Leaderboard(file_path='./savefiles/leaderboard_perma_eval_sample.json')
 
+  all_leaderboards = [leaderboard_random_1, leaderboard_random_2, leaderboard_random_3, leaderboard_random_4,
+                      leaderboard_random_5, leaderboard_random_6, leaderboard_perma_eval_choice,
+                      leaderboard_perma_eval_sample]
+
+  # create divisions
+  # TODO: rename league to division
+  divi_random_1 = RandomDivision('savefiles/division_random_1.json', game_engine, leaderboard_random_1, agent_manager)
+  divi_random_2 = RandomDivision('savefiles/division_random_2.json', game_engine, leaderboard_random_2, agent_manager)
+  divi_random_3 = RandomDivision('savefiles/division_random_3.json', game_engine, leaderboard_random_3, agent_manager)
+  divi_random_4 = RandomDivision('savefiles/division_random_4.json', game_engine, leaderboard_random_4, agent_manager)
+  divi_random_5 = RandomDivision('savefiles/division_random_5.json', game_engine, leaderboard_random_5, agent_manager)
+  divi_random_6 = RandomDivision('savefiles/division_random_6.json', game_engine, leaderboard_random_6, agent_manager)
+  divi_perma_eval_choice = PermaEvalChoiceDivision('savefiles/division_perma_eval_choice.json', game_engine,
+                                                   leaderboard_perma_eval_choice, agent_manager)
+  divi_perma_eval_sample = PermaEvalSampleDivision('savefiles/division_perma_eval_sample.json', game_engine,
+                                                   leaderboard_perma_eval_sample, agent_manager)
+
+  all_divi = [divi_random_1, divi_random_2, divi_random_3, divi_random_4, divi_random_5, divi_random_6,
+              divi_perma_eval_choice, divi_perma_eval_sample]
+
+  # load / add agents
   agent_manager.load()
-  leaderboard_1.load()
-  division_1.load()
 
+  # add_all_students(agent_manager, division_random_1, students=('qlearn1',))
+  # add_all_students(agent_manager, division_random_2, students=('qlearn2',))
+  # add_all_students(agent_manager, division_random_3, students=('qlearn3',))
+  # add_all_students(agent_manager, division_random_4, students=('qlearn1', 'qlearn2'))
+  # add_all_students(agent_manager, division_random_5, students=('qlearn1', 'qlearn2', 'qlearn3'))
+  # add_all_students(agent_manager, division_random_6, students=('qlearn1', 'qlearn2', 'qlearn3'))
+
+  # add_all_baselines(agent_manager, division_random_1)
+  # add_all_baselines(agent_manager, division_random_2)
+  # add_all_baselines(agent_manager, division_random_3)
+  # add_all_baselines(agent_manager, division_random_4)
+  # add_all_baselines(agent_manager, division_random_5)
+  # division_random_6.state['teachers'].append('36536334')
+  # agent_manager._add_agent_with_id('36536334', 'qlearn1', False)
+
+  # load leaderboards
+  for l in all_leaderboards: l.load()
+
+  # load divisions
+  for d in all_divi: d.load()
+
+  # agent_manager.save()
+
+  # GAMELOOP
   round_idx = 0
   while (True):
-    for _ in range(N_ROUNDS_TO_CLONE):
-      division_1.run_next()
-      round_idx += 1
-      print('[runner] round {:>5} finished \n{}'.format(round_idx, '-' * 100))
-    division_1.clone_mutables()
-    agent_manager.save()
-    leaderboard_1.save()
-    division_1.save()
 
+    # run_next of divisions
+    for d in all_divi: d.run_next()
 
-def add_all_agents(agent_manager):
-  # add agents
-  students = ['qlearn1', 'qlearn2', 'qlearn3']
-  for s in students:
-    id = agent_manager.add_agent(s, True)
-    division_1.agents['students'].append(id)
+    round_idx += 1
+    print('[runner] round {:>5} finished \n{}'.format(round_idx, '-' * 100))
 
-  teachers = ['call', 'random']
-  for t in teachers:
-    id = agent_manager.add_agent(t, False)
-    division_1.agents['teachers'].append(id)
+    if (round_idx % N_ROUNDS_TO_UPDATE_LEADERBOARD) == 0:
+      # save leaderboards realtime
+      for l in all_leaderboards: l.save()
 
+    if (round_idx % N_ROUNDS_TO_CLONE) == 0:
+      # call clone
+      divi_random_1.clone_mutables()
+      divi_random_2.clone_mutables()
+      divi_random_3.clone_mutables()
+      divi_random_4.clone_mutables()
+      divi_random_5.clone_mutables()
+      divi_random_6.clone_mutables()
 
-#
-# TODO: remove
-#
+      # save agent manager
+      agent_manager.save()
 
-def pick_with_probability(n, elems, probs):
-  picks = []
-  assert (n <= len(elems))
-  for _ in range(n):
-    assert (len(elems) == len(probs))
-    total_prob = sum(probs)
-    cumulative_probs = []
-    for p in probs:
-      cumulative_probs.append(p / total_prob + sum(cumulative_probs[-1:]))
-    assert (len(cumulative_probs) == len(elems))
-    r = random.random()
-    for i, p in enumerate(cumulative_probs):
-      if p >= r:
-        idx = i
-        break
-    picks.append(elems[idx])
-    del elems[idx]
-    del probs[idx]
-  return picks
-
-
-def generate_baseline_matchup(all_agent_types):
-  available_matchup_infos = [(a_class, a_info) for a_class in all_agent_types for a_info in a_class.available_agents()]
-
-  available_learners = [(a_class, a_info) for a_class, a_info in available_matchup_infos
-                        if a_info[1]['matchup_info']['type'] == 'learner']
-
-  # random select learners
-  learners = random.sample(available_learners, 1)
-
-  # set baselines
-  call_bots = [(CallAgentNP, ('CallAgent-{}.0'.format(i), {'type': 'teacher'})) for i in range(1, 3)]
-  random_bots = [(RandomAgentNP, ('RandomAgent-{}.0'.format(i), {'type': 'teacher'})) for i in range(1, 4)]
-
-  classroom = [*learners, *call_bots, *random_bots]
-  instantiated = [a_type.from_id(a_info[0]) for a_type, a_info in classroom]
-  return instantiated
-
-
-def generate_weighted_matchup(all_agent_types, rating, n_learners, n_teachers, p_only_top_teachers):
-  available_matchup_infos = [(a_class, a_info) for a_class in all_agent_types for a_info in a_class.available_agents()]
-
-  available_learners = [(a_class, a_info) for a_class, a_info in available_matchup_infos
-                        if a_info[1]['matchup_info']['type'] == 'learner']
-  available_teachers = [(a_class, a_info) for a_class, a_info in available_matchup_infos
-                        if a_info[1]['matchup_info']['type'] == 'teacher']
-
-  # random select learners
-  learners = random.sample(available_learners, n_learners)
-
-  # trueskill weighted select
-  sorted_teachers = [(rating.get_rating_from_id(a_info[0])['mu'], a_class, a_info)
-                     for a_class, a_info in available_teachers]
-
-  # encourage use of new agents
-  for i, st in enumerate(sorted_teachers):
-    if st[0] == 100:
-      sorted_teachers[i] = (1000, sorted_teachers[i][1], sorted_teachers[i][2])
-
-  sorted_teachers.sort(key=lambda x: x[0], reverse=True)
-  top_teachers = sorted_teachers[:n_teachers]
-  rest_teachers = sorted_teachers[n_teachers:]
-
-  if random.random() < p_only_top_teachers:
-    n_from_top = n_teachers
-    n_from_rest = 0
-  else:
-    n_from_top = random.randrange(0, n_teachers)
-    n_from_rest = n_teachers - n_from_top
-
-  picked_top_teachers = pick_with_probability(n_from_top, [(a_class, a_info) for _, a_class, a_info in top_teachers],
-                                              [rating for rating, _, _ in top_teachers])
-
-  picked_rest_teachers = pick_with_probability(n_from_rest, [(a_class, a_info) for _, a_class, a_info in rest_teachers],
-                                               [rating for rating, _, _ in rest_teachers])
-
-  classroom = [*picked_top_teachers, *picked_rest_teachers, *learners]
-  random.shuffle(classroom)
-
-  instantiated = [a_type.from_id(a_info[0]) for a_type, a_info in classroom]
-  return instantiated
-
-
-def generate_random_teacher_matchup(all_agent_types, n_players):
-  available_matchup_infos = [(a_class, a_info) for a_class in all_agent_types for a_info in a_class.available_agents()]
-
-  available_teachers = [(a_class, a_info) for a_class, a_info in available_matchup_infos
-                        if a_info[1]['matchup_info']['type'] == 'teacher']
-
-  ages = [a_info[1]['matchup_info']['age'] for _, a_info in available_teachers]
-  max_age = max(ages)
-  classroom = pick_with_probability(n_players, available_teachers, [max_age - age + 1 for age in ages])
-
-  instantiated = [a_type.from_id(a_info[0]) for a_type, a_info in classroom]
-  return instantiated
+      # save divisions
+      for d in all_divi: d.save()
