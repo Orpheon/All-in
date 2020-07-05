21a451111e6d84d0788140faa1a3577a0b87b460
Orpheon <alex.liebendorf@gmail.com>
2020-07-05 19:44:46 +0200

    More small implementations to strategy vector.

--- strategy/compute_strat_vector.py
+++ strategy/compute_strat_vector.py
@@ -58,13 +58,13 @@ def compute_strat_vector(agent):
   # Fixed
   N_ROUNDS = 4
   N_SEATING_POSITIONS = 6
-  N_ACTIVE_PLAYERS = 5
+  N_MAX_ACTIVE_PLAYERS = 5
   N_PLAYERS_TOTAL = 6
 
   n_games = np.ndarray((
     N_SEATING_POSITIONS,
     N_ROUNDS,
-    N_ACTIVE_PLAYERS,
+    N_MAX_ACTIVE_PLAYERS,
     N_OWN_POT_SIZE_BINS,
     N_TOTAL_POT_BINS,
     N_CARD_PERCENTILE_BINS
@@ -72,7 +72,7 @@ def compute_strat_vector(agent):
   strategy = np.ndarray((
     N_SEATING_POSITIONS,
     N_ROUNDS,
-    N_ACTIVE_PLAYERS,
+    N_MAX_ACTIVE_PLAYERS,
     N_OWN_POT_SIZE_BINS,
     N_TOTAL_POT_BINS,
     N_CARD_PERCENTILE_BINS,
@@ -89,69 +89,78 @@ def compute_strat_vector(agent):
 
   evaluator = treys.Evaluator()
 
-  # TODO: While not enough samples
-  for round in range(N_ROUNDS):
-    n_community_cards = [0, 3, 4, 5][round]
-    for seating_position in range(N_SEATING_POSITIONS):
-      for unfolded_players in range(1, N_ACTIVE_PLAYERS):
-        for own_pot in [i/N_OWN_POT_SIZE_BINS for i in range(N_OWN_POT_SIZE_BINS)]:
-          community_cards, all_hole_cards = generate_cards(N_PLAYERS_TOTAL, BATCH_SIZE)
-          hole_cards = all_hole_cards[:, seating_position, :]
-          rank_bin = np.zeros(BATCH_SIZE)
+  lowest_bin_count = np.amin(n_games)
+  while lowest_bin_count <= N_MIN_SAMPLES_PER_BIN:
+    for round in range(N_ROUNDS):
+      n_community_cards = [0, 3, 4, 5][round]
+      for seating_position in range(N_SEATING_POSITIONS):
+        for n_active_players in range(1, N_MAX_ACTIVE_PLAYERS):
+          for own_pot_idx in range(N_OWN_POT_SIZE_BINS):
+            own_pot = own_pot_idx / N_OWN_POT_SIZE_BINS
+            community_cards, all_hole_cards = generate_cards(N_PLAYERS_TOTAL, BATCH_SIZE)
+            hole_cards = all_hole_cards[:, seating_position, :]
+            rank_bin = np.zeros(BATCH_SIZE)
 
-          folded_players = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL - N_ACTIVE_PLAYERS))
-          active_players = np.zeros((BATCH_SIZE, N_ACTIVE_PLAYERS))
-          last_raiser = np.zeros(BATCH_SIZE)
-          for i in range(BATCH_SIZE):
-            folded_row = random.choice([x for x in range(N_PLAYERS_TOTAL) if x != seating_position], N_PLAYERS_TOTAL - N_ACTIVE_PLAYERS)
-            active_row = [x for x in range(N_PLAYERS_TOTAL) if x not in folded_row]
-            folded_players[i, :] = folded_row
-            active_players[i, :] = active_row
-            last_raiser[i] = random.choice(active_row)
+            folded_players = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL - n_active_players))
+            active_players = np.zeros((BATCH_SIZE, n_active_players))
+            last_raiser = np.zeros(BATCH_SIZE)
+            for i in range(BATCH_SIZE):
+              folded_row = random.choice([x for x in range(N_PLAYERS_TOTAL) if x != seating_position], N_PLAYERS_TOTAL - n_active_players)
+              active_row = [x for x in range(N_PLAYERS_TOTAL) if x not in folded_row]
+              folded_players[i, :] = folded_row
+              active_players[i, :] = active_row
+              last_raiser[i] = random.choice(active_row)
+              if round != constants.PRE_FLOP:
+                rank = evaluator.evaluate(community_cards[i, :n_community_cards].tolist(), hole_cards[i].tolist())
+                percentile = 1 - ranktable[rank, round - constants.FLOP]
+                rank_bin[i] = int(percentile * N_CARD_PERCENTILE_BINS)
+              else:
+                pass
+                # TODO preflop card ranking
+
+            current_bets = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
+            prev_round_investment = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
+
             if round != constants.PRE_FLOP:
-              rank = evaluator.evaluate(community_cards[i, :n_community_cards].tolist(), hole_cards[i].tolist())
-              percentile = 1 - ranktable[rank, round - constants.FLOP]
-              rank_bin[i] = int(percentile * N_CARD_PERCENTILE_BINS)
+              prev_round_investment[folded_players] = np.random.random(size=folded_players.shape) * own_pot
+              portions = np.random.random(size=(BATCH_SIZE, round + 1))
+              # If we are the last raiser, then we raised in the last round, which means there are no bets standing this round
+              portions[last_raiser == seating_position, -1] = 0
+              portions /= np.sum(portions, axis=1)
+              prev_round_investment[active_players] = np.sum(portions[:, :-1], axis=1) * own_pot
+              current_bets[active_players] = portions[:, -1] * own_pot
             else:
-              pass
-              # TODO preflop card ranking
+              current_bets[folded_players] = np.random.random(size=folded_players.shape) * own_pot
+              current_bets[active_players] = own_pot
 
-          current_bets = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
-          prev_round_investment = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
+            for i in range(BATCH_SIZE):
+              if last_raiser[i] < seating_position:
+                for active_player in active_players[i, active_players[i].index(last_raiser[i]) : active_players[i].index(seating_position)]:
+                  current_bets[i, active_player] += MIN_RAISE
+              else:
+                for active_player in active_players[i, active_players[i].index(seating_position):] + active_players[i, :active_players[i].index(last_raiser[i])]:
+                  current_bets[i, active_player] += MIN_RAISE
 
-          if round != constants.PRE_FLOP:
-            prev_round_investment[folded_players] = np.random.random(size=folded_players.shape) * own_pot
-            portions = np.random.random(size=(BATCH_SIZE, round + 1))
-            # If we are the last raiser, then we raised in the last round, which means there are no bets standing this round
-            portions[last_raiser == seating_position, -1] = 0
-            portions /= np.sum(portions, axis=1)
-            prev_round_investment[active_players] = np.sum(portions[:, :-1], axis=1) * own_pot
-            current_bets[active_players] = portions[:, -1] * own_pot
-          else:
-            current_bets[folded_players] = np.random.random(size=folded_players.shape) * own_pot
-            current_bets[active_players] = own_pot
+            folded_1h = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
+            folded_1h[folded_players] = 1
 
-          for i in range(BATCH_SIZE):
-            if last_raiser[i] < seating_position:
-              for active_player in active_players[i, active_players[i].index(last_raiser[i]) : active_players[i].index(seating_position)]:
-                current_bets[i, active_player] += MIN_RAISE
-            else:
-              for active_player in active_players[i, active_players[i].index(seating_position):] + active_players[i, :active_players[i].index(last_raiser[i])]:
-                current_bets[i, active_player] += MIN_RAISE
+            actions, amounts = agent.act(
+              player_idx=seating_position,
+              round=round,
+              active_rounds=np.ones(BATCH_SIZE),
+              current_bets=current_bets,
+              min_raise=np.full(BATCH_SIZE, MIN_RAISE),
+              prev_round_investment=prev_round_investment,
+              folded=folded_1h,
+              last_raiser=last_raiser,
+              hole_cards=hole_cards,
+              community_cards=community_cards[:, :n_community_cards]
+            )
+            # TODO: Handle actions and amounts somehow
 
-          folded_1h = np.zeros((BATCH_SIZE, N_PLAYERS_TOTAL))
-          folded_1h[folded_players] = 1
+            total_pot_idx = int(np.sum(current_bets + prev_round_investment, axis=1) * N_TOTAL_POT_BINS / N_PLAYERS_TOTAL)
 
-          actions, amounts = agent.act(
-            player_idx=seating_position,
-            round=round,
-            active_rounds=np.ones(BATCH_SIZE),
-            current_bets=current_bets,
-            min_raise=np.full(BATCH_SIZE, MIN_RAISE),
-            prev_round_investment=prev_round_investment,
-            folded=folded_1h,
-            last_raiser=last_raiser,
-            hole_cards=hole_cards,
-            community_cards=community_cards[:, :n_community_cards]
-          )
-          # TODO: Handle actions and amounts somehow
+            for k in range(N_CARD_PERCENTILE_BINS):
+              n_games[seating_position, round, n_active_players, own_pot_idx, total_pot_idx, k] += np.count_nonzero(rank_bin == k)
+
+    lowest_bin_count = np.amin(n_games)
