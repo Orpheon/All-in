b39172b4489c0203110ce7fde5bd13cbec2adf2f
Jens <jens.kaminsky@students.fhnw.ch>
2020-07-06 20:15:55 +0200

    added leaderboard ab plot, added similar divi

--- league/leaderboard.py
+++ league/leaderboard.py
@@ -3,6 +3,7 @@ from trueskill import TrueSkill
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import pandas as pd
 
 from league.agentManager import AGENT_TYPES, AgentManager
 
@@ -14,40 +15,50 @@ class Leaderboard:
 
   def __init__(self, file_path):
     self.FILE_PATH = file_path
-    self._trueskill = TrueSkill(mu=TRUESKILL_START_MU, sigma=TRUESKILL_START_SIGMA)
-    self._ratings = {'current': {}, 'history': []}
     self.changed = False
 
   def load(self):
     with open(self.FILE_PATH, 'r') as f:
       data = json.load(f)
     self._ratings = data
-    print('[Leaderboard <- {}]: loaded {} rankings over {} games'.format(self.FILE_PATH, len(self._ratings['current']),
-                                                                         len(self._ratings['history'])))
+    print('[Leaderboard <- {}]: load {}'.format(self.FILE_PATH, ' '.join('{}: {} |'.format(key, len(val))
+                                                                         for key, val in self._ratings.items())))
     self.changed = False
 
   def save(self):
     if self.changed:
       with open(self.FILE_PATH, 'w') as f:
         json.dump(self._ratings, f)
-      print('[Leaderboard -> {}]: saved {} rankings over {} games'.format(self.FILE_PATH, len(self._ratings['current']),
-                                                                          len(self._ratings['history'])))
+      print('[Leaderboard -> {}]: save {}'.format(self.FILE_PATH, ' '.join('{}: {} |'.format(key, len(val))
+                                                                           for key, val in self._ratings.items())))
     self.changed = False
 
-  def update_from_placings(self, agents):
-    current_ratings = [self._trueskill.Rating(*self._ratings['current'].get(agent, ())) for agent in agents]
+
+class LeaderboardTS(Leaderboard):
+
+  def __init__(self, file_path):
+    super().__init__(file_path)
+    self._trueskill = TrueSkill(mu=TRUESKILL_START_MU, sigma=TRUESKILL_START_SIGMA)
+    self._ratings = {'current': {}, 'history': []}
+
+  def update_from_placings(self, agent_ids):
+    current_ratings = [self._trueskill.Rating(*self._ratings['current'].get(agent, ())) for agent in agent_ids]
     new_rating_groups = self._trueskill.rate([(cr,) for cr in current_ratings])
     hist_append = {}
-    for agent, new_rating_group in zip(agents, new_rating_groups):
+    for agent, new_rating_group in zip(agent_ids, new_rating_groups):
       new_rating = (new_rating_group[0].mu, new_rating_group[0].sigma)
       self._ratings['current'][agent] = new_rating
       hist_append[agent] = new_rating
     self._ratings['history'].append(hist_append)
     self.changed = True
 
-  def get_rankings(self):
+  def get_all_rankings(self):
     sorted_ratings = sorted(self._ratings['current'].items(), key=lambda x: x[1][0], reverse=True)
     return sorted_ratings
+
+  def get_ranking(self, agent_id):
+    # TODO ranking != mu, but keep atm
+    return self._ratings['current'].get(agent_id, (TRUESKILL_START_MU, TRUESKILL_START_SIGMA))
 
   def reset_rankings(self):
     self._ratings = {'current': {}, 'history': []}
@@ -99,14 +110,91 @@ class Leaderboard:
     print(output)
 
 
+class LeaderboardPL(Leaderboard):
+
+  def __init__(self, file_path):
+    super().__init__(file_path)
+    self._ratings = {'current': {}, 'n_games': {}, 'agent_ids': []}
+
+  def update_from_placings(self, agent_ids):
+
+    for rank, agent_a in enumerate(agent_ids):
+      for agent_b in agent_ids:
+        if agent_a != agent_b:
+          if agent_a not in self._ratings['agent_ids']: self._ratings['agent_ids'].append(agent_a)
+          if agent_b not in self._ratings['agent_ids']: self._ratings['agent_ids'].append(agent_b)
+
+          if agent_a not in self._ratings['n_games']: self._ratings['n_games'][agent_a] = {}
+          if agent_b not in self._ratings['n_games'][agent_a]: self._ratings['n_games'][agent_a][agent_b] = 0
+
+          if agent_a not in self._ratings['current']: self._ratings['current'][agent_a] = {}
+          if agent_b not in self._ratings['current'][agent_a]: self._ratings['current'][agent_a][agent_b] = 0
+
+          self._ratings['n_games'][agent_a][agent_b] += 1
+          n_games = self._ratings['n_games'][agent_a][agent_b]
+          old_r = self._ratings['current'][agent_a][agent_b]
+          self._ratings['current'][agent_a][agent_b] = (rank - old_r) / n_games + old_r
+
+    self.changed = True
+
+  def plot_matrix(self):
+    ids = self._ratings['agent_ids']
+
+    current = self._ratings['current']
+    ids_sorted_by_avg_rank = sorted(ids, key=lambda x: sum(current[x].values()) / len(current[x].values()))
+
+    n = len(ids)
+    tmp_arr = np.zeros((n, n))
+
+    tmp_arr.fill(np.nan)
+    data_frame = pd.DataFrame(tmp_arr)
+    rename_dict = {idx: a_id for idx, a_id in enumerate(ids_sorted_by_avg_rank)}
+    data_frame.rename(columns=rename_dict, index=rename_dict, inplace=True)
+
+    for agent_a in self._ratings['current'].keys():
+      for agent_b, val in self._ratings['current'][agent_a].items():
+        data_frame[agent_a][agent_b] = val
+
+    sns.set(style='white')
+    cmap = sns.color_palette("coolwarm", 15)
+    sns.heatmap(data_frame, cmap=cmap)
+    plt.xlabel('agent')
+    plt.ylabel('opponent')
+    plt.show()
+
+  def print_leaderboard(self):
+    agent_ids = self._ratings['agent_ids']
+    current = self._ratings['current']
+
+    ids_with_avg_rank = (
+      (i,
+       sum(current[i].values()) / len(current[i].values()),
+       np.median(list(current[i].values())),
+       np.percentile(list(current[i].values()), 80))
+      for i in agent_ids)
+    ids_sorted_by_avg_rank = sorted(ids_with_avg_rank, key=lambda x: x[3])
+
+    print('{:>3}: {:<8} {:<5} {:<5} {:<5}'.format(' ', 'agent_id', 'avg', 'med', '80pctl'))
+    output = "\n".join('{:>3}: {:<8} {:<5.2f} {:<5.2f} {:<5.2f}'.format(idx + 1, agent_id, avg_rank, median_rank, pctl)
+                       for idx, (agent_id, avg_rank, median_rank, pctl) in enumerate(ids_sorted_by_avg_rank))
+    print(output)
+
+
 if __name__ == '__main__':
+  lpl = LeaderboardPL('../savefiles/leaderboard_matrix.json')
+  lpl.load()
+  lpl.plot_matrix()
+  lpl.print_leaderboard()
+
   agent_manager = AgentManager('../savefiles/agent_manager.json', None)
   agent_manager.load()
 
-  for l in ['perma_eval_choice', 'perma_eval_sample'] + ['random_{}'.format(i) for i in range(1,7)]:
+  relevant_leaderboards = [
+    'perma_eval_similar']  # + ['perma_eval_choice', 'perma_eval_sample'] + ['random_{}'.format(i) for i in range(7,8)]
 
+  for l in relevant_leaderboards:
     # select leaderboard to watch
-    leaderboard = Leaderboard('../savefiles/leaderboard_{}.json'.format(l))
+    leaderboard = LeaderboardTS('../savefiles/leaderboard_{}.json'.format(l))
     leaderboard.load()
 
     ratings = [r[0] for r in leaderboard._ratings['current'].values()]
