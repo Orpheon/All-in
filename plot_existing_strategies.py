9e9218f62f8aa9e6dc227a9e59a170e3b23297af
Orpheon <alex.liebendorf@gmail.com>
2020-07-13 23:29:58 +0200

    Added kmeans and removed the hacked rank plotting.

--- plot_existing_strategies.py
+++ plot_existing_strategies.py
@@ -15,7 +15,11 @@ sns.set_style('whitegrid')
 
 image_path = os.path.join("strategy", "images")
 
-agent_manager = AgentManager(file_path='./savefiles/agent_manager.json', models_path='./models')
+agent_manager = AgentManager(
+  file_path='./savefiles/agent_manager.json',
+  models_path='./models',
+  possible_agent_names=None
+)
 agent_manager.load()
 
 def label_point(x, y, val, ax):
@@ -23,11 +27,11 @@ def label_point(x, y, val, ax):
   for i, point in a.iterrows():
     ax.text(point['x'] + .02, point['y'], str(point['val']))
 
-def draw_traditional_metrics(agent_manager, hue='type', filename='traditional_scatterplot.png'):
+def draw_traditional_metrics(agent_manager, hue='type'):
   metrics = pd.DataFrame()
 
-  with open("leaderboard_perma_eval_similar.json", "r") as f:
-    rankings = json.load(f)['current']
+  # with open("leaderboard_perma_eval_similar.json", "r") as f:
+  #   rankings = json.load(f)['current']
 
   for agent_id, agent_info in agent_manager.get_all_agents():
     if not agent_info.TRAINABLE:
@@ -35,14 +39,17 @@ def draw_traditional_metrics(agent_manager, hue='type', filename='traditional_sc
       aggression = traditional_metrics.compute_aggression(strategy)
       tightness = traditional_metrics.compute_tightness(strategy)
       metrics = metrics.append({
+        'agent': agent_info.AGENT_NAME + "-" + agent_id,
         'tightness': tightness,
         'normed_aggression': aggression / (1 + aggression) if np.isfinite(aggression) else 1,
+        'aggression': aggression,
         'type': agent_info.AGENT_TYPE,
-        'rank': rankings[agent_id][0]
+        'division': agent_info.ORIGIN_DIVI
+        # 'rank': rankings[agent_id][0]
       }, ignore_index=True)
 
   plt.figure(figsize=(8, 8))
-  if hue == 'type':
+  if hue == 'type' or hue == 'division':
     palette = sns.color_palette("husl", metrics[hue].nunique())
     ax = sns.scatterplot(data=metrics, x='tightness', y='normed_aggression', hue=hue, palette=palette)
   elif hue == 'rank':
@@ -57,14 +64,56 @@ def draw_traditional_metrics(agent_manager, hue='type', filename='traditional_sc
   os.makedirs(image_path, exist_ok=True)
   ax.axhline(y=0.5, color='k')
   ax.axvline(x=traditional_metrics.tightness_border(), color='k')
-  plt.savefig(os.path.join(image_path, filename), bbox_inches='tight')
+  plt.savefig(os.path.join(image_path, "traditional_scatterplot_"+hue+".png"), bbox_inches='tight')
   plt.clf()
+
+def draw_traditional_metrics_kmeans(agent_manager):
+  metrics = pd.DataFrame()
+
+  strategies = {}
+  for agent_id, agent_info in agent_manager.get_all_agents():
+    if not agent_info.TRAINABLE:
+      strategy = agent_manager.get_strategy_vector(agent_id)
+      strategies[agent_id] = strategy
+
+  max_n_clusters = 6
+  cluster_attribution = multidimensional_scaling.kmeans(strategies, MAX_N=max_n_clusters)
+
+  for n_clusters in range(2, max_n_clusters):
+    for agent_id, agent_info in agent_manager.get_all_agents():
+      if not agent_info.TRAINABLE:
+        strategy = agent_manager.get_strategy_vector(agent_id)
+        aggression = traditional_metrics.compute_aggression(strategy)
+        tightness = traditional_metrics.compute_tightness(strategy)
+        metrics = metrics.append({
+          'agent': agent_info.AGENT_NAME + "-" + agent_id,
+          'tightness': tightness,
+          'normed_aggression': aggression / (1 + aggression) if np.isfinite(aggression) else 1,
+          'aggression': aggression,
+          'cluster': cluster_attribution[n_clusters][agent_id]
+        }, ignore_index=True)
+
+    plt.figure(figsize=(8, 8))
+    palette = sns.color_palette("husl", metrics['cluster'].nunique())
+    ax = sns.scatterplot(data=metrics, x='tightness', y='normed_aggression', hue='cluster', palette=palette)
+
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.xlabel("Tightness")
+    plt.ylabel("Normed Aggression")
+    plt.title("Aggression vs Tightness clustered, all agents")
+    # label_point(metrics.tightness, metrics.normed_aggression, metrics.agent, ax)
+    os.makedirs(image_path, exist_ok=True)
+    ax.axhline(y=0.5, color='k')
+    ax.axvline(x=traditional_metrics.tightness_border(), color='k')
+    plt.savefig(os.path.join(image_path, "traditional_scatterplot_kmeans_"+str(n_clusters)+".png"), bbox_inches='tight')
+    plt.clf()
 
 def draw_mds(agent_manager):
   strategies = {}
 
-  with open("leaderboard_perma_eval_similar.json", "r") as f:
-    rankings = json.load(f)['current']
+  # with open("leaderboard_perma_eval_similar.json", "r") as f:
+  #   rankings = json.load(f)['current']
 
   for agent_id, agent_info in agent_manager.get_all_agents():
     if not agent_info.TRAINABLE:
@@ -79,7 +128,8 @@ def draw_mds(agent_manager):
       'x': v[0],
       'y': v[1],
       'type': name.split("-")[0],
-      'rank': rankings[name.split("-")[1]][0]
+      'division': agent_manager.get_info(name.split("-")[1]).ORIGIN_DIVI
+      # 'rank': rankings[name.split("-")[1]][0]
     }, ignore_index=True)
 
   plt.figure(figsize=(8, 8))
@@ -91,20 +141,30 @@ def draw_mds(agent_manager):
   plt.savefig(os.path.join(image_path, "mds_type.png"), bbox_inches='tight')
   plt.clf()
 
+  # plt.figure(figsize=(8, 8))
+  # ax = sns.scatterplot(data=data, x='x', y='y', hue="rank")
+  # plt.title("Multidimensional scaling, all agents")
+  # # label_point(data.x, data.y, data.agent, ax)
+  # plt.savefig(os.path.join(image_path, "mds_rank.png"), bbox_inches='tight')
+  # plt.clf()
+
   plt.figure(figsize=(8, 8))
-  ax = sns.scatterplot(data=data, x='x', y='y', hue="rank")
+  palette = sns.color_palette("husl", data.division.nunique())
+  ax = sns.scatterplot(data=data, x='x', y='y', hue="division", palette=palette)
   plt.title("Multidimensional scaling, all agents")
   # label_point(data.x, data.y, data.agent, ax)
-  plt.savefig(os.path.join(image_path, "mds_rank.png"), bbox_inches='tight')
+  os.makedirs(image_path, exist_ok=True)
+  plt.savefig(os.path.join(image_path, "mds_division.png"), bbox_inches='tight')
   plt.clf()
+
 
 def draw_aggression_vs_cardrank(agent_manager):
   agent_types = agent_manager.get_all_agent_types()
   palette = sns.color_palette("husl", len(agent_types))
   agent_types = {t: palette[idx] for idx, t in enumerate(agent_types)}
 
-  with open("leaderboard_perma_eval_similar.json", "r") as f:
-    rankings = json.load(f)['current']
+  # with open("leaderboard_perma_eval_similar.json", "r") as f:
+  #   rankings = json.load(f)['current']
 
   for round_idx,round in enumerate(['preflop', 'flop', 'turn', 'river']):
     for agent_id, agent_info in agent_manager.get_all_agents():
@@ -135,8 +195,8 @@ def draw_raise_size_vs_cardrank(agent_manager):
   palette = sns.color_palette("husl", len(agent_types))
   agent_types = {t: palette[idx] for idx, t in enumerate(agent_types)}
 
-  with open("leaderboard_perma_eval_similar.json", "r") as f:
-    rankings = json.load(f)['current']
+  # with open("leaderboard_perma_eval_similar.json", "r") as f:
+  #   rankings = json.load(f)['current']
 
   for round_idx, round in enumerate(['preflop', 'flop', 'turn', 'river']):
     for agent_id, agent_info in agent_manager.get_all_agents():
@@ -163,8 +223,10 @@ def draw_raise_size_vs_cardrank(agent_manager):
     plt.savefig(os.path.join(image_path, "money_vs_rank_{}.png".format(round)), bbox_inches='tight')
     plt.clf()
 
-# draw_traditional_metrics(agent_manager, hue='type', filename="traditional_scatterplot_type.png")
+# draw_traditional_metrics(agent_manager, hue='type')
+# draw_traditional_metrics(agent_manager, hue='division')
 # draw_traditional_metrics(agent_manager, hue='rank', filename="traditional_scatterplot_rank.png")
-draw_mds(agent_manager)
+# draw_mds(agent_manager)
 # draw_aggression_vs_cardrank(agent_manager)
 # draw_raise_size_vs_cardrank(agent_manager)
+draw_traditional_metrics_kmeans(agent_manager)
--- strategy/multidimensional_scaling.py
+++ strategy/multidimensional_scaling.py
@@ -1,6 +1,8 @@
 import numpy as np
 import constants
 import sklearn.manifold
+import sklearn.cluster
+import pandas as pd
 
 def condense(strategy):
   shape = strategy.shape
@@ -14,7 +16,7 @@ def condense(strategy):
 
   return strat_condensed
 
-def distance(strategy1, strategy2):
+def distance_bhattacharyya(strategy1, strategy2):
   strat1_condensed = condense(strategy1)
   strat2_condensed = condense(strategy2)
 
@@ -24,15 +26,24 @@ def distance(strategy1, strategy2):
 
   return -np.log(bhattacharyya_coeff)
 
+def distance_euclidean(strategy1, strategy2):
+  strat1_condensed = condense(strategy1)
+  strat2_condensed = condense(strategy2)
+
+  distance = np.linalg.norm(strat1_condensed - strat2_condensed)
+  return distance
+
 def compute_mds(strategies, verbose=True):
   # Build distance matrix first
   strategy_tuples = list(strategies.items())
   dist_matrix = np.zeros((len(strategies), len(strategies)))
   for idx1,(agent1, strat1) in enumerate(strategy_tuples):
     for idx2,(agent2, strat2) in enumerate(strategy_tuples):
-      dist = distance(strat1, strat2)
+      # dist = distance_bhattacharyya(strat1, strat2)
+      dist = distance_euclidean(strat1, strat2)
       if dist == None:
-        print("Agents {0} and {1} have zero in common, throwing both".format(agent1, agent2))
+        if verbose:
+          print("Agents {0} and {1} have zero in common, throwing both".format(agent1, agent2))
         new_dict = dict(strategy_tuples)
         del new_dict[agent1]
         del new_dict[agent2]
@@ -40,7 +51,8 @@ def compute_mds(strategies, verbose=True):
       dist_matrix[idx1, idx2] = dist
       dist_matrix[idx2, idx1] = dist
     dist_matrix[idx1, idx1] = 0
-    print("MDS: {}%".format(round(100 * idx1 / len(strategy_tuples), 2)))
+    if verbose:
+      print("MDS: {}%".format(round(100 * idx1 / len(strategy_tuples), 2)))
 
   mds = sklearn.manifold.MDS(
     n_components=2,
@@ -49,3 +61,16 @@ def compute_mds(strategies, verbose=True):
   embedding = mds.fit(dist_matrix).embedding_
 
   return {key: embedding[idx].tolist() for idx, (key, value) in enumerate(strategy_tuples)}
+
+def kmeans(strategies, MAX_N=12, verbose=True):
+  strategy_tuples = list(strategies.items())
+  results = pd.DataFrame(index=[x[0] for x in strategy_tuples])
+  flattened_strats = np.array([x[1].flatten() for x in strategy_tuples])
+  for n in range(2, MAX_N):
+    if verbose: print(n, "clusters on", MAX_N - 1)
+    kmeans = sklearn.cluster.KMeans(n_clusters=n)
+    clusters = kmeans.fit_predict(X=flattened_strats)
+    results[n] = clusters
+
+  print(results)
+  return results
